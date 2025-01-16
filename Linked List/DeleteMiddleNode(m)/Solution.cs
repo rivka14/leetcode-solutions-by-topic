6e11af87b75f98510535fdd5764b1efc2f25d4a2
Rivka Weiss <rw8881234@gmail.com>
2025-01-16 20:49:17 +0200

    Create Solution.cs

---
+++ Linked List/DeleteMiddleNode(m)/Solution.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public ListNode DeleteMiddle(ListNode head) {
+        // If the list is empty or has only one node, return null
+        if (head == null || head.next == null)
+            return null;
+        // Pointer slow starts from the head
+        ListNode slow = new ListNode();
+        slow.next = head;
+        // Pointer fast starts from the head
+        ListNode fast = head;
+        // Move slow pointer one step at a time and fast pointer two steps
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // Skip the middle node by pointing the previous node to the next node
+        ListNode node = slow.next;
+        slow.next = slow.next.next;
+        // Explicitly delete the node from memory
+        DeleteNode(node);   
+        return head;
+    }
+  
+    // Verify deletion of the node from memory (If it is necessary)
+    public void DeleteNode(ListNode node) {
+        node.next = null;
+    }
+}
