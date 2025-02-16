62e5e27fe5a26a9a1124677d91cb4bfa54d5a4a0
Rivka Weiss <rw8881234@gmail.com>
2025-02-16 16:57:07 +0200

    Create Solution.py

---
+++ Stack/RemovingStarsFromAString(m)/Solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeStars(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+        
+        for c in s:
+            if c != '*':
+                stack.append(c)
+            elif stack:
+                stack.pop()
+        
+        return ''.join(stack)
+
