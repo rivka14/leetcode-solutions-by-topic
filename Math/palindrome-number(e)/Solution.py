d18ede8a6709f3c2ae214d5c9d3cef74081679d3
Rivka Weiss <rw8881234@gmail.com>
2026-07-02 23:57:47 +0300

    docs: add Palindrome Number

---
+++ Math/palindrome-number(e)/Solution.py
@@ -0,0 +1,10 @@
+def isPalindrome(x):
+    if x < 0:
+        return False
+    original = x
+    reversed_num = 0
+    while x > 0:
+        digit = x % 10
+        reversed_num = reversed_num * 10 + digit
+        x //= 10
+    return original == reversed_num
